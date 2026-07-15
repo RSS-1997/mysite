1100d3f854e402f7651b1f2d973a1dbc4377055a
RSS-1997 <z.saeidi99@gmail.com>
2026-07-15 12:07:46 +0330

    test(templatetags): add experimental custom template tag examples
    - add temporary test URL and view
    - add test template for experimentation
    - implement sample custom template filters
    - implement sample simple tags
    - implement sample inclusion tags
    - use experimental code for learning and validation

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,3 +1,23 @@
 from django import template
+from blog.models import Post
 
 register = template.Library()
+
+@register.simple_tag(name='totalposts')
+def function():
+    posts = Post.objects.filter(status=1).count()
+    return posts
+
+@register.simple_tag(name='posts')
+def function():
+    posts = Post.objects.filter(status=1)
+    return posts
+
+@register.filter
+def snippet(value, arg=20):
+    return value[:arg] + "..."
+
+@register.inclusion_tag('popular-posts.html')
+def popular_posts():
+    posts =  Post.objects.filter(status=1).order_by('-published_date')[:1]
+    return {'posts':posts}
